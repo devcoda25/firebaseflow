53ed85e15e89412d5781f10626b3925adbd3644b
Dev <devcoda25@gmail.com>
2025-08-15 16:00:46 +0000

    lets remove the grid in the canvas

--- src/components/CanvasWithLayoutWorker/CanvasWithLayoutWorker.tsx
+++ src/components/CanvasWithLayoutWorker/CanvasWithLayoutWorker.tsx
@@ -152,7 +152,6 @@ function InnerCanvas({
           fitView
           proOptions={{ hideAttribution: true }}
         >
-          <Background variant={BackgroundVariant.Lines} gap={GRID_SIZE} style={{backgroundColor: 'hsl(var(--background))', stroke: 'hsla(var(--border), 0.5)', strokeWidth: 0.5}} />
           <Controls className={styles.controls} />
           <MiniMap pannable zoomable />
           <div style={{ position: 'absolute', inset: 0, pointerEvents: 'none', zIndex: 10 }}>
